5d4809a0c0201db48a6ddee6811f356f55435c08
lakshmikant hunachal <lakshmikanthunachal@gmail.com>
2026-09-13 19:33:29 +0530

    variables file added

---
+++ variables.py
@@ -0,0 +1,23 @@
+#python variables:
+# In Python, a variable is a name that refers to a value stored in memory. Variables are used to store data that can be manipulated and accessed throughout a program. You can assign values to variables using the assignment operator (=). For example:
+# Assigning a value to a variable
+x = 10
+y = 5
+#type of variables:
+# In Python, variables can hold different types of data, such as integers, floats, strings, lists, dictionaries, and more. The type of a variable is determined by the value assigned to it. You can check the type of a variable using the type() function. For example:
+# Checking the type of variables    
+print(type(x))  # Output: <class 'int'>
+print(type(y))  # Output: <class 'int'>
+ #rules for naming variables:
+# 1. Variable names must start with a letter (a-z, A-Z) or an underscore (_).
+# 2. The rest of the variable name can contain letters, digits (0-9), and underscores.
+# 3. Variable names are case-sensitive, meaning that "myVariable" and "myvariable" are considered different variables.
+# 4. Variable names cannot be the same as Python keywords (reserved words). 
+
+#how find out the length of the variable:
+# You can find the length of a variable (if it is a string, list, or other iterable) using the len() function. The len() function returns the number of items in an object. For example:
+# Finding the length of a string variable   
+#example:
+my_string = "Hello, World!" 
+print(len(my_string))  # Output: 13
+
