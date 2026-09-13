9eef554ec60303460540c5010321309d58731a6e
lakshmikant hunachal <lakshmikanthunachal@gmail.com>
2026-09-13 19:26:10 +0530

    introduction to python

---
+++ input_and_output.py
@@ -0,0 +1,4 @@
+#input function:
+# The input() function allows the user to provide input to the program. It reads a line from the input (usually from the user), converts it into a string, and returns it.
+name = input("Enter your name: ")
+print("Hello, " + name + "!")
---
+++ introduction.py
@@ -0,0 +1,39 @@
+# Python is a high-level programming language known for its simple and readable syntax. It has the following features:
+
+# Allows writing clean code with fewer lines.
+# Supports multiple programming paradigms including object-oriented, functional and procedural programming.
+# Widely used in web development, automation, data analysis, artificial intelligence and many other fields.
+# Dynamically typed and has automatic garbage collection
+
+#print hello world using Python
+print("Hello, World!")
+
+# print() is a built-in Python function that instructs the computer to display text on the screen.
+
+# "# this is a comment" is a comment in Python. Comments are used to explain the code and are ignored by the interpreter.
+ 
+ #Application of Python in various fields:
+# 1. Web Development: Python has frameworks like Django and Flask that make it easy to build web applications.
+# 2. Data Analysis: Libraries like Pandas and NumPy allow for efficient data manipulation and analysis.
+# 3. Artificial Intelligence: Python has libraries like TensorFlow and PyTorch that are widely used for machine learning and deep learning applications.  
+# 4. Automation: Python can be used to automate repetitive tasks, such as web scraping and file management.
+
+#python application in real life:
+# 1. Instagram: The popular social media platform uses Python for its backend services. 
+# 2. Spotify: The music streaming service uses Python for data analysis and backend services. 
+# 3. Dropbox: The cloud storage service uses Python for its desktop client and server-side code.
+# 4. Reddit: The social news aggregation and discussion website uses Python for its backend services.
+# 5. YouTube: The video-sharing platform uses Python for various backend services and data analysis tasks.etc
+
+#advantages of Python:
+# 1. Easy to Learn and Use: Python's simple syntax makes it accessible for beginners.
+# 2. Versatile: Python can be used for a wide range of applications, from web development to data science.
+# 3. Large Community: Python has a large and active community that contributes to its development and provides support through forums and tutorials.                    
+# 4. Extensive Libraries: Python has a vast collection of libraries and frameworks that simplify development tasks.
+
+
+#disadvantages of Python:
+# 1. Slower Execution Speed: Python is an interpreted language, which can make it slower than compiled languages like C or C++.
+# 2. Not Ideal for Mobile Development: Python is not commonly used for mobile app development   
+# 3. Global Interpreter Lock (GIL): Python's GIL can be a limitation for multi-threaded applications, as it prevents multiple native threads from executing Python bytecodes at once.   
+#4. Memory Consumption: Python's memory consumption can be higher compared to other languages, which may be a concern for memory-intensive applications.
